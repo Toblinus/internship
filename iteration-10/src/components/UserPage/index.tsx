3358304ecce0f357793009c26419c91eaf601eff
Toblinus <anatoliy.blinkov3006@gmail.com>
2020-12-29 20:13:45 +0300

    iteration-10

--- iteration-10/src/components/UserPage/index.tsx
+++ iteration-10/src/components/UserPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 
 import GHConnector from '../../helpers/GithubConnector';
 import Profile from '../Profile';
@@ -7,17 +7,6 @@ import FullScreenError from '../FullScreenError';
 
 type propsType = {
     username: string
-}
-
-type stateType = {
-    isLoad: boolean,
-    avatar_url: string,
-    user_url: string,
-    isError: boolean,
-    repos: {
-        name: string,
-        url: string
-    }[]
 }
 
 const UserPage: FC<propsType> = function(props){
@@ -67,65 +56,6 @@ const UserPage: FC<propsType> = function(props){
             />
         )
     );
-}
-
-class UserPage1 extends Component<propsType, stateType>{
-    constructor(props: propsType) {
-        super(props);
-        this.state = {
-            isLoad: true,
-            avatar_url: '',
-            user_url: '',
-            isError: false,
-            repos: []
-        }
-    }
-
-    componentDidMount(){
-        GHConnector.getUser(this.props.username)
-            .then((user) => {          
-                if(user) {
-                    this.setState({
-                        isLoad: false,
-                        isError: false,
-                        avatar_url: user.avatar_url,
-                        user_url: user.html_url
-                    });
-                    return GHConnector.getRepos(this.props.username)
-                } else {
-                    this.setState({
-                        isError: true,
-                        isLoad: false
-                    });
-                }
-                
-            })
-            .then((data) => {
-                const repos = [];
-                if(data) {
-                    data.forEach(repo => { repos.push({
-                        name: repo.name,
-                        url: repo.html_url
-                    }) });
-                }
-                this.setState({ repos });
-            })
-    }
-
-    render() {
-        return this.state.isLoad ? (<Spinner />) : (
-            this.state.isError ? (
-                <FullScreenError header="404" description="Not found" />
-            ) : (
-                <Profile 
-                    username={this.props.username}
-                    avatar={this.state.avatar_url}
-                    repos={this.state.repos}
-                    user_url={this.state.user_url}
-                />
-            )
-        );
-    }
 }
 
 export default UserPage;
