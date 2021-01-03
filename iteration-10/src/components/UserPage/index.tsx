606f534a3e8b06fc6426243b2e7d015b117f9118
Toblinus <anatoliy.blinkov3006@gmail.com>
2021-01-03 14:52:02 +0300

    custom hook

--- iteration-10/src/components/UserPage/index.tsx
+++ iteration-10/src/components/UserPage/index.tsx
@@ -1,58 +1,41 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC } from 'react';
 
 import GHConnector from '../../helpers/GithubConnector';
 import Profile from '../Profile';
 import Spinner from '../Spinner';
 import FullScreenError from '../FullScreenError';
+import { useLoader, StatusLoader } from '../../hooks/useLoader';
 
 type propsType = {
     username: string
 }
 
 const UserPage: FC<propsType> = function(props){
-    const [isLoad, setIsLoad] = useState(true);
-    const [avatarUrl, setAvatarUrl] = useState('');
-    const [userUrl, setuserUrl] = useState('');
-    const [isError, setIsError] = useState(false);
-    const [repos, setRepos] = useState([]);
+    const [status, data] = useLoader(
+        GHConnector.getUser(props.username), 
+        [props.username]
+    );
 
-    useEffect(() => {
-        if(isLoad)
-        GHConnector.getUser(props.username)
-            .then((user) => {          
-                if(user) {
-                    setAvatarUrl(user.avatar_url);
-                    setuserUrl(user.html_url);
-                    setIsError(false);
-                    setIsLoad(false);
-                    return GHConnector.getRepos(props.username)
-                } else {
-                    setIsError(true);
-                    setIsLoad(false);
-                }
-                
-            })
-            .then((data) => {
-                const repos = [];
-                if(data) {
-                    data.forEach(repo => { repos.push({
-                        name: repo.name,
-                        url: repo.html_url
-                    }) });
-                }
-                setRepos(repos);
-            })
-    });
+    const [reposStatus, reposData] = useLoader(
+        GHConnector.getRepos(props.username),
+        [props.username]
+    );
 
-    return isLoad ? (<Spinner />) : (
-        isError ? (
+    const repos = reposData?.map(repo => ({
+        name: repo.name,
+        url: repo.html_url
+    }));
+
+    return (status == StatusLoader.loading || 
+            reposStatus == StatusLoader.loading) ? (<Spinner />) : (
+        (status == StatusLoader.hasError) ? (
             <FullScreenError header="404" description="Not found" />
         ) : (
             <Profile 
                 username={props.username}
-                avatar={avatarUrl}
+                avatar={data.avatar_url}
                 repos={repos}
-                user_url={userUrl}
+                user_url={data.html_url}
             />
         )
     );
