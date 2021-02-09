e2764e68454e01a9db9fd00c25c2cfa4045397a4
toblinus <anatoliy,blinkov3006@gmail.com>
2021-02-09 19:57:32 +0300

    trello

--- src/components/WrapperWithButtons/index.jsx
+++ src/components/WrapperWithButtons/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './style.css';
+
 const WrapperWithButtons = ({ children, buttons }) => {
     return (<div className='wrap-with-btns'>
-        <div className='wrap-with-btns__buttons'>{ buttons }</div>
+        <div className='wrap-with-btns__buttons'>{ 
+            buttons.map((btn, index) => <button
+                 onClick={btn.action}
+                 key={index}
+                 className='wrap-with-btns__button'>{ 
+                    btn.content 
+                }</button>)
+        }</div>
         <div className='wrap-with-btns__content'>{ children }</div>
     </div>);
 }
 
 WrapperWithButtons.propTypes = {
     children: PropTypes.element.isRequired,
-    buttons: PropTypes.node
+    buttons: PropTypes.arrayOf(PropTypes.exact({
+        content: PropTypes.node.isRequired,
+        action: PropTypes.func.isRequired
+    }))
 }
 
 export default WrapperWithButtons;
--- src/components/WrapperWithButtons/index.stories.js
+++ src/components/WrapperWithButtons/index.stories.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import WWB from './index';
+import { action } from "@storybook/addon-actions";
 
-import Card from '../Card';
+import CardComponent from '../Card';
+import CardListComponent from '../CardList';
+import BoardComponent from '../Board';
 
 export default {
-    title: 'WrapperWithButtons',
+    title: 'Trello/WrapperWithButtons',
     component: WWB
 }
 
 const Template = (args) => <WWB {...args} />;
 
-export Card = Template.bind({});
+export const Card = Template.bind({});
+Card.args = {
+    children: <CardComponent header='this is title' text='this is content' />,
+    buttons: [{
+        content: 'h',
+        action: action('click')
+    }, {
+        content: 'f',
+        action: action('click')
+    }]
+};
+
+export const CardList = Template.bind({});
+CardList.args = {
+    children: <CardListComponent header='this is title' />,
+    buttons: [{
+        content: 'h',
+        action: action('click')
+    }, {
+        content: 'f',
+        action: action('click')
+    }]
+};
+
+export const Board = Template.bind({});
+Board.args = {
+    children: <BoardComponent header='this is title' />,
+    buttons: [{
+        content: '+',
+        action: action('click')
+    }, {
+        content: 'f',
+        action: action('click')
+    }]
+};
