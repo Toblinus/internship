7a710f615e62ecc4734ff84f45057515bb37a8fa
toblinus <anatoliy,blinkov3006@gmail.com>
2021-02-18 15:22:57 +0300

    adding test

--- src/components/ActionsBar/ActionBarBtn/index.spec.js
+++ src/components/ActionsBar/ActionBarBtn/index.spec.js
@@ -1,12 +1,20 @@
 import { shallow } from 'enzyme';
 import ActionBarBtn from './index';
 
-describe('Testing <ActionBarBtn />', () => {
-    const mockFn = jest.fn();
-    const onClick = new mockFn();
-
-    const elm = shallow(<ActionBarBtn onClick={onClick}>
-        <div></div>
-    </ActionBarBtn>)
-    console.log(elm.find('div').debug())
+it('Testing <ActionBarBtn />', () => {
+    const forTest = [
+        <div></div>,
+        'Text'
+    ];
+    const onClick = jest.fn();
+    forTest.map((test) => {
+        const elm = shallow(<ActionBarBtn onClick={() => onClick()}>
+            { test }
+        </ActionBarBtn>)
+        elm.simulate('click', {});
+        expect(elm.contains(test)).toBe(true)
+    })
+   // const received = onClick.mock.calls.length;
+    const expected = forTest.length;
+    expect(onClick.mock.calls.length).toBe(expected)
 })
